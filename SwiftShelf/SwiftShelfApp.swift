f1704e1a088a04abeeec2ce650b48b3a422a28bd
Michael Vinci <michaeldvinci@gmail.com>
2025-08-02 15:22:46 -0500

    Initial Commit

---
+++ SwiftShelf/SwiftShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftShelfApp.swift
+//  SwiftShelf
+//
+//  Created by Michael Vinci on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftShelfTests/SwiftShelfTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftShelfTests.swift
+//  SwiftShelfTests
+//
+//  Created by Michael Vinci on 8/2/25.
+//
+
+import Testing
+@testable import SwiftShelf
+
+struct SwiftShelfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
